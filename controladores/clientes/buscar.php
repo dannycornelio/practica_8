f67f44bd9429a63d74e99bc75d7dde33b6749714
dannycornelio <licdannycornelio@gmail.com>
2023-05-31 19:28:44 -0600

    correccionn

--- controladores/clientes/buscar.php
+++ controladores/clientes/buscar.php
@@ -4,11 +4,7 @@ try {
     $cliente = new Cliente($_GET);
     
     $clientes = $cliente->buscar();
-    // echo "<pre>";
-    // var_dump($clientes);
-    // echo "</pre>";
-    // exit;
-    // $error = "NO se guardó correctamente";
+
 } catch (PDOException $e) {
     $error = $e->getMessage();
 } catch (Exception $e2){
